fb51b8a41be1f0c2e7fc546d95f4e1932bfc96c2
zaobai <zhou.yu@yanwei365.com>
2019-10-24 15:18:03 +0800

    add

--- data-structures-and-algorithms/src/test/java/com/example/demo/ListTest.java
+++ data-structures-and-algorithms/src/test/java/com/example/demo/ListTest.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
+
 public class ListTest {
 
-    /*
-    1->2->3->4->5
-    5->4->3->2->1
-     */
 
-    public static void main(String[] args) {
+    @Test
+    public void testReverse() {
+        /*
+            1->2->3->4->5
+            5->4->3->2->1
+     */
         ListNode one = new ListNode(1);
         ListNode two = new ListNode(2);
         ListNode three = new ListNode(3);
@@ -23,9 +26,72 @@ public class ListTest {
         print(reverse(one));
     }
 
+    @Test
+    public void testIsPalindrome() {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(2);
+        ListNode five = new ListNode(1);
+//        ListNode oo = new ListNode(1);
+
+        one.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = five;
+//        five.next = oo;
+
+        System.out.println(isPalindrome(one));
+    }
+
+    @Test
+    public void testRemoveNthFromEnd() {
+        /*
+            1->2->3->4->5
+            1->2->3->5
+     */
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+
+        one.next = two;
+        /*two.next = three;
+        three.next = four;
+        four.next = five;*/
+
+        print(one);
+        print(removeNthFromEnd(one, 2));
+    }
+
+    // 删除链表的倒数第N个节点
+    // https://blog.csdn.net/biezhihua/article/details/79889987
+    private ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode frontNode = head;
+        ListNode backNode = head;
+
+        for (int i = 0; i < n; i++) {
+            frontNode = frontNode.next;
+        }
+
+        if (frontNode == null) {
+            return backNode.next;
+        }
+
+        while (frontNode.next != null) {
+            frontNode = frontNode.next;
+            backNode = backNode.next;
+        }
+        backNode.next = backNode.next.next;
+
+        return head;
+    }
+
+
     // 单链表反转
     // https://www.jianshu.com/p/bd6a64d36916
-    public static ListNode reverse(ListNode head) {
+    private static ListNode reverse(ListNode head) {
         if (head == null) {
             return null;
         }
@@ -67,7 +133,7 @@ public class ListTest {
         return pre;
     }
 
-    public static void print(ListNode head) {
+    private static void print(ListNode head) {
         if (head == null) {
             return;
         }
@@ -79,5 +145,67 @@ public class ListTest {
         }
 
     }
+
+    // 回文链表
+    // https://github.com/andavid/leetcode-java/blob/master/note/234/README.md
+    private static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+
+            /*
+            1->2->3->2->1
+
+            fast = 1.next.next = 3
+            next = 1.next = 2
+            1.next = null
+            prev = 1
+            slow = 2
+
+
+            fast = 3.next.next = 1
+            next = 2.next = 3
+            2.next = 1
+            prev = 2
+            slow = 3
+
+             */
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+            /*
+             slow = 2;
+             */
+        }
+
+        while (slow != null) {
+            /*
+            slow = 2
+            prev = 2
+             */
+            if (slow.val != prev.val) {
+                return false;
+            }
+            slow = slow.next;
+            prev = prev.next;
+        }
+
+        return true;
+
+    }
+
+
 
 }
