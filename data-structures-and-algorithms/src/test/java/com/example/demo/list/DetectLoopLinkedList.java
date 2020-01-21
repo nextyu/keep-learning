50908dd8d3bf4eba4a713a786d9f984e38356416
zaobai <zhou.yu@yanwei365.com>
2020-01-21 17:10:26 +0800

    add list test

--- data-structures-and-algorithms/src/test/java/com/example/demo/list/DetectLoopLinkedList.java
+++ data-structures-and-algorithms/src/test/java/com/example/demo/list/DetectLoopLinkedList.java
@@ -2,6 +2,14 @@ package com.example.demo.list;
 
 import com.example.demo.ListNode;
 
+/**
+ *
+ * Floyd's Cycle-Finding Algorithm
+ *
+ * https://codingfreak.blogspot.com/2012/09/detecting-loop-in-singly-linked-list_22.html
+ * https://xbuba.com/questions/2663115
+ *
+ */
 public class DetectLoopLinkedList {
     public static void main(String[] args) {
         ListNode one = new ListNode(1);
@@ -14,16 +22,23 @@ public class DetectLoopLinkedList {
         two.next = three;
         three.next = four;
         four.next = five;
-//        five.next = two;
 
-        ListNode fast = one;
-        ListNode slow = one;
+        five.next = two; // 循环
 
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode hare = one;
+        ListNode tortoise = one;
 
-            if (slow == fast) {
+        /*
+        乌龟走一步，野兔走两步，如果存在循环链表，乌龟和野兔就回相遇，野兔跑的快些，多跑了n圈
+         */
+
+
+        while (hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+
+            if (tortoise == hare) {
+                System.out.println(tortoise.val);
                 System.out.println(true);
                 return;
             }
